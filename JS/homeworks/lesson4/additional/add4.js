e75b203ba7291fca5effd5eea38d626c5ff6c152
VyacheslavKolos <v.kolos@nltu.lviv.ua>
2021-11-19 11:03:09 +0200

    additional-4

---
+++ JS/homeworks/lesson4/additional/add4.js
@@ -0,0 +1,73 @@
+// - створити функцію, яка якщо приймає один аргумент, просто вивдоить його, якщо два аргументи - складає або конкатенує їх між собою.
+// function someFunction() {
+//     let res;
+//     if (typeof arguments[0] === 'number' && typeof arguments[1] === "number") {
+//         res = 0;
+//     } else {
+//         res = "";
+//     }
+//
+//     if (arguments.length === 1) {
+//         console.log(arguments[0]);
+//     } else {
+//         for (let argument of arguments) {
+//             res += argument;
+//         }
+//     }
+//     console.log(res);
+// }
+//
+// someFunction(2, "Hi")
+
+
+// - створити функцію  яка приймає два масиви та скаладає значення елементів з однаковими індексами  та повертає новий результуючий масив.
+//     EXAMPLE:
+// [1,2,3,4]
+//     [2,3,4,5]
+// результат
+//     [3,5,7,9]
+// function concatArr(arr, arr1) {
+//     let newArr=[];
+//     for (let i = 0; i < arr.length; i++) {
+//         newArr[i]=arr[i]+arr1[i];
+//     }
+//     return newArr;
+// }
+// arr=[1,2,3,4];
+// arr1=[2,3,4,5];
+// let resultConcatArr=concatArr(arr,arr1);
+// console.log(resultConcatArr);
+
+
+// - Створити функцію яка приймає масив будь яких объектів, та повертає масив ключів всіх обєктів
+// EXAMPLE:
+//     [{name: 'Dima', age: 13}, {model: 'Camry'}]  ===> [ name, age, model ]
+// function returnKeys(arr) {
+//     let arrKeys=[];
+//     for (const arrElement of arr) {
+//         for (const key in arrElement) {
+//             arrKeys.push(key);
+//         }
+//     }
+//     return arrKeys;
+// }
+// let arrObj= [{name: 'Dima', age: 13}, {model: 'Camry'}];
+// resultReturnKeys=returnKeys(arrObj);
+// console.log(resultReturnKeys);
+
+
+//     - Створити функцію яка приймає масив будь яких объектів, та повертає масив значень всіх обєктів
+// EXAMPLE:
+//     [{name: 'Dima', age: 13}, {model: 'Camry'}]  ===> [ Dima, 13, Camry ]
+// function returnValues(arr) {
+//     let arrValues=[];
+//     for (const arrElement of arr) {
+//         for (const key in arrElement) {
+//             arrValues.push(arrElement[key]);
+//         }
+//     }
+//     return arrValues;
+// }
+// let arrObj= [{name: 'Dima', age: 13}, {model: 'Camry'}];
+// resultReturnValues=returnValues(arrObj);
+// console.log(resultReturnValues);
