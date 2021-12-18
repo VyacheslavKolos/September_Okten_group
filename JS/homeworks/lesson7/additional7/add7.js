b0911a997716578108ddd1f5ae1de6c1aa6c12bc
VyacheslavKolos <v.kolos@nltu.lviv.ua>
2021-12-18 15:39:59 +0200

    add7

--- JS/homeworks/lesson7/additional7/add7.js
+++ JS/homeworks/lesson7/additional7/add7.js
@@ -130,13 +130,8 @@
 // let card1 = userCard(1,100,100,[]);
 // let card2=userCard(2,100,1000,[]);
 
-let help=[];
 
-let counter=0;
 function userCard(number) {
-    help.push(number);
-
-
     let card = {key: number, balance: 100, transactionLimit: 100, historyLogs: []};
 
         return {
